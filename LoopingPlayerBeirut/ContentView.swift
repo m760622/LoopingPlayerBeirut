d2f45a81c8823796eee701589eb3e0572859b719
Mohammed Abunada <m7606225@gmail.com>
2020-08-04 23:23:26 +0200

    Initial Commit

---
+++ LoopingPlayerBeirut/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  LoopingPlayerBeirut
+//
+//  Created by Mohammed Abunada on 2020-08-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoopingPlayerBeirut/LoopingPlayerBeirutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopingPlayerBeirutApp.swift
+//  LoopingPlayerBeirut
+//
+//  Created by Mohammed Abunada on 2020-08-04.
+//
+
+import SwiftUI
+
+@main
+struct LoopingPlayerBeirutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
